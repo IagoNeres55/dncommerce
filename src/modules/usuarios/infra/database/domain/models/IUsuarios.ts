24737c73b3671d003dec6524c183c187b70a5d87
IagoNeres55 <iagoneres90@gmail.com>
2025-03-21 08:42:59 -0300

    feat: created entity user

---
+++ src/modules/usuarios/infra/database/domain/models/IUsuarios.ts
@@ -0,0 +1,9 @@
+export type IUsuarios = {
+  id: number
+  name: string
+  email: string
+  password: string
+  perfil: string
+  created_at: Date
+  updated_at: Date
+}
---
+++ src/modules/usuarios/infra/database/entities/Usuarios.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { IUsuarios } from '../domain/models/IUsuarios'
+
+export enum Perfil {
+  ADMIN = 'admin',
+  USER = 'user',
+  MODERADOR = 'moderador',
+}
+
+@Entity('usuarios')
+export class Usuarios implements IUsuarios {
+  @PrimaryGeneratedColumn('uuid')
+  id: number
+
+  @Column({ type: 'text' })
+  name: string
+
+  @Column({ type: 'text' })
+  email: string
+
+  @Column({ type: 'text' })
+  password: string
+
+  @Column({ type: 'enum', enum: Perfil, default: Perfil.USER })
+  perfil: string
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date
+}
