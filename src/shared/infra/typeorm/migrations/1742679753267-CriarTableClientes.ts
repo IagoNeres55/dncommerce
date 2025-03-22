3c82c2d0c044403fe9fc6ab1724ef012d6c950d0
IagoNeres55 <iagoneres90@gmail.com>
2025-03-22 19:24:13 -0300

    feat: create migration clientes

---
+++ src/shared/infra/typeorm/migrations/1742679753267-CriarTableClientes.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CriarTableClientes1742679753267 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+      await queryRunner.createTable(
+        new Table({
+          name: 'clientes',
+          columns: [
+            {
+              name: 'id',
+              type: 'integer',
+              isPrimary: true,
+              isGenerated: true,
+              generationStrategy: 'increment'
+            },
+            {
+              name: 'name',
+              type: 'varchar'
+            },
+            {
+              name: 'email',
+              type: 'varchar',
+              isUnique: true
+            },
+            {
+              name: 'telefone',
+              type: 'varchar'
+            },
+            {
+              name: 'endereco',
+              type: 'varchar'
+            },
+            {
+              name: 'usuario_id',
+              type: 'integer'
+            },
+            {
+              name: 'created_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+            {
+              name: 'updated_at',
+              type: 'timestamp',
+              default: 'now()',
+            },
+
+          ],
+          foreignKeys: [
+            {  name: 'cliente_usuarios',
+              referencedTableName: 'usuarios',
+              referencedColumnNames: ['id'],
+              columnNames: ['usuario_id'],
+              onDelete: 'CASCADE',
+              onUpdate: 'CASCADE'
+            }
+
+          ]
+        })
+      )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('clientes')
+    }
+
+}
