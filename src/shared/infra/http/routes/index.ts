b7ca5dfa48598037c97458d2bb0bb6909cb0cbd6
IagoNeres55 <iagoneres90@gmail.com>
2025-07-31 09:29:12 -0300

    feat: create route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,8 @@
 import ClienteRoute from '@modules/clientes/infra/http/routes/clienteRoute'
 import UsuariosRoutes from '@modules/usuarios/infra/http/routes/usuarioRoutes'
 
+import ProdutosRoutes from '@modules/produtos/infra/http/routes/produtosRoutes'
+
 import { Router } from 'express'
 
 const routes = Router()
@@ -8,5 +10,7 @@ const routes = Router()
 routes.use('/usuario', UsuariosRoutes)
 
 routes.use('/clientes', ClienteRoute)
+
+routes.use('/produtos', ProdutosRoutes)
 
 export default routes
